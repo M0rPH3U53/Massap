2ba6ab0e8644160371b80c3d9bb0ecfc8af4633f
M0rPH3U53 <144029241+M0rPH3U53@users.noreply.github.com>
2025-02-03 22:34:00 +0100

    Update massap.sh

--- massap.sh
+++ massap.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Config Reseau
 gateway='10.20.30.1'
 mac='AC:86:BD:5E:90:8C'
 
@@ -9,18 +10,22 @@ port='ports.txt'
 read -p 'Entrer une IP: ' IP
 read -p 'Entrer un rate: ' rate
 
+# Scan les 65535 ports
 masscan $IP -p- --rate $rate --router-ip $gateway --router-mac $mac -oG $mass
 
+# Verifie si des ports sont ouvers
 if [ ! -s "$mass" ]; then
     echo "Aucun port ouvert"
     exit 1
 else
-    # Extraire les ports ouverts et les enregistrer dans $port
     grep "open" $mass | awk '{print $7}' | cut -d'/' -f1 > $port
 fi
 
+# Scan les ports d'apres les resultat de massap & genere un rapport
 nmap -sS -A -sC -p $(cat $port | tr '\n' ',') --script vuln -v -oX $IP-tcp.xml $IP
 
+# Converti le .xml en .html
 xsltproc $IP-tcp.xml > $IP-tcp.html
 
+# Supprime les fichier txt apres scan
 rm $port $mass
